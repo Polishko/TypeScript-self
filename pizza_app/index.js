f5dd122b22de57850f1f0a85dc41dd5e4fb60190
Gizmo <nalanbilgin@gmail.com>
2025-02-08 14:26:41 +0200

    Initial commit

---
+++ pizza_app/index.js
@@ -0,0 +1,36 @@
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
+
+let cashInRegister = 100;
+let nextOrderId = 1;
+let orderQueue = [];
+
+function addNewpizzaObj(pizzaObj) {
+  menu.push(pizzaObj);
+}
+
+function placeOrder(pizzaName) {
+  const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+  if (selectedPizza) {
+    cashInRegister += selectedPizza.price;
+    const newOrder = {
+      id: nextOrderId++,
+      pizza: selectedPizza,
+      status: "ordered",
+    };
+    orderQueue.push(newOrder);
+  }
+
+  return selectedPizza;
+}
+
+function completeOrder(orderId) {
+  let orderItem = orderQueue.find((orderObj) => orderObj === orderId);
+  if (orderItem) orderItem.status = "completed";
+
+  return orderItem;
+}
